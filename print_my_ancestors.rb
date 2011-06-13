38b9cb723d26d6b38cf06c2c0d358252dad769ca
Jim Lindstrom <jim.lindstrom@gmail.com>
2011-06-13 07:33:32 -0400

    Added a readme.  Made print_my_ancestors.rb accept a command-line-arg for the starting person

--- print_my_ancestors.rb
+++ print_my_ancestors.rb
@@ -6,10 +6,10 @@ require './familytree/persondb'
 require './familytree/treehelpers'
 
 
-#db_type = :pstore
-#db_opts = {:filename => "person_db.pstore"}
-db_type = :postgres
-db_opts = {:host => 'localhost',
+#db_opts = {:type => :pstore, :filename => "person_db.pstore"}
+
+db_opts = {:type => :postgres,
+           :host => 'localhost',
            :port => nil,
            :options => nil,
            :tty => nil,
@@ -17,10 +17,10 @@ db_opts = {:host => 'localhost',
            :user => 'jim',
            :pass => 'password'}
 
-@person_db = FamilyTree::PersonDB.create(db_type, db_opts)
-#myancestors = @person_db.ancestors_of("Dean Randall Lindstrom")
-myancestors = @person_db.ancestors_of("James Brian Lindstrom", 20)
-#myancestors = @person_db.ancestors_of("Jennifer Robins Bernstein")
+@person_db = FamilyTree::PersonDB.create(db_opts)
+
+starting_person = ARGV.shift
+myancestors = @person_db.ancestors_of(starting_person, 20)
 
 puts '<html>'
 puts '<head>'
